3550ec489db486fead581790f2e7dd674cbb385c
Alex Mulgrew <alexpmulgrew@gmail.com>
2022-01-10 16:36:49 +0000

    Task 3 complete

--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -1,22 +1,19 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 function PokemonViewer({ id }) {
   // TODO: send http request to `https://pokeapi.co/api/v2/pokemon/${id}` and display the data!
   // HINT: you will need useState and useEffect!
 
-  const [pokemonData, setPokemonData] = useState([])
+  const [pokemonData, setPokemonData] = useState([]);
 
- 
-
-
-  useEffect(  () => {
+  useEffect(() => {
     async function getData() {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
       const data = await response.json();
       setPokemonData(data);
-    }; 
+    }
     getData();
-  },[id, pokemonData])
+  }, [id, pokemonData]);
 
   // useEffect(() => {
   //   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
@@ -31,8 +28,12 @@ function PokemonViewer({ id }) {
     <div className="pokemon-viewer">
       <p>display pokemon with id {id} here!</p>
       <p>Name: {pokemonData.name}</p>
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+        alt={pokemonData.name}
+      ></img>
     </div>
   );
 }
-
+//{"front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"}
 export default PokemonViewer;
